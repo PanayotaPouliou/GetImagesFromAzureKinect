45c2f1d336d1cacbdc81747306d98d7a6fe1449d
PanayotaPouliou <yotapouliou@gmail.com>
2021-10-21 11:58:44 +0200

    depth

--- check.py
+++ check.py
@@ -1,6 +1,7 @@
 import cv2
 import os
-from depthFunctions import get_world_coord, get_Z, get_data
+from depthFunctions import get_world_coord, get_Z
+from capture import get_data
 import PIL
 from PIL import Image
 import math
@@ -10,13 +11,13 @@ import numpy as np
 path= 'C:\\Users\\ppou\\source\\repos\\pyKinectAzure\\dData'
 get_data(path, 'no', 10)
 
-subj_coordinates = 832,151
-obj_coordinates = 1152,151
+#subj_coordinates = 832,151
+#obj_coordinates = 1152,151
 
-subj_depth = get_Z(path, subj_coordinates)
-obj_depth = get_Z(path, obj_coordinates)
+#subj_depth = get_Z(path, subj_coordinates)
+#obj_depth = get_Z(path, obj_coordinates)
 
-D, A, B = get_world_coord(path, subj_coordinates, obj_coordinates)
+#D, A, B = get_world_coord(path, subj_coordinates, obj_coordinates)
 
 #print(subj_depth, obj_depth, d)
-print(D,A,B)
+#print(D,A,B)
