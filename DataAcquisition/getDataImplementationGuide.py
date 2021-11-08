49809598374dcf08e5fa74dba4b37c80e80f40d9
PanayotaPouliou <yotapouliou@gmail.com>
2021-11-08 11:33:11 +0100

    cleanup

--- DataAcquisition/getDataImplementationGuide.py
+++ DataAcquisition/getDataImplementationGuide.py
@@ -2,14 +2,11 @@ from os import path
 from capture_data_functions import get_data, create_folder,timestamp
 
 
-#in_path= 'C:\\Users\\ppou\\Downloads\\gitclone\\sg_benchmark\\DataAcquisition\\filesaving' 
-in_path='C:\\Users\\ppou\\source\\repos\\pyKinectAzure\\DataAcquisition\\filesaving'
+in_path= 'C:\\Users\\ppou\\Downloads\\gitclone\\sg_benchmark\\data\\input\\RGBD' 
+#in_path='C:\\Users\\ppou\\source\\repos\\pyKinectAzure\\DataAcquisition\\filesaving'
 
 #New Folder Creation
 path = create_folder(in_path, 'CITA')
-
-#New metadata.csv file creation
-#f=create_MetaData(path, 'CITA')
 
 #Save files in the created folder
 get_data(path, 'no', 10)
