414e5c7fc1a9276928cdf323bc9561daa942a678
Panayota Pouliou <yotapouliou@gmail.com>
2021-11-11 11:48:59 +0100

    RWC crosscheck

--- RealCoordinates/implementationGuide.py
+++ RealCoordinates/implementationGuide.py
@@ -2,10 +2,10 @@ from os import path
 from getRealCoords import get_world_coord#, get_Z if we want !only! the depth
 from PIL import Image
 
-img_depth= Image.open('C:\\Users\\ppou\\source\\repos\\pyKinectAzure\\filesaving\\CITA\\2021-11-01 11꞉29꞉44 d.png')
+img_depth= Image.open('RealCoordinates\RWC_check\D_20211110113510.png')
 
-subj_coordinates = 832,151
-obj_coordinates = 1152,151
+subj_coordinates = 1464,464
+obj_coordinates = 1145,752
 
 #subj_depth = get_Z(img_depth, subj_coordinates)
 #obj_depth = get_Z(img_depth, obj_coordinates)
